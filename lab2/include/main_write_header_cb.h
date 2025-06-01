4f3bfeecb7cacf61f743b178b61f46f154cc974f
ea239 <zzh3329869117@gmail.com>
2025-06-01 15:49:05 -0400

    update

--- lab2/include/main_write_header_cb.h
+++ lab2/include/main_write_header_cb.h
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <curl/curl.h>
-#include <pthread.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include "png_op.h"
 
+#define IMG_URL "http://ece252-1.uwaterloo.ca:2520/image?img=1"
+#define DUM_URL "https://example.com/"
+#define ECE252_HEADER "X-Ece252-Fragment: "
+#define BUF_SIZE 1048576  /* 1024*1024 = 1M */
+#define BUF_INC  524288   /* 1024*512  = 0.5M */
 
+#define max(a, b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
 typedef struct recv_buf2 {
     char *buf;       /* memory to hold a copy of received data */
@@ -18,19 +24,8 @@ typedef struct recv_buf2 {
                      /* <0 indicates an invalid seq number */
 } RECV_BUF;
 
-#define max(a, b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
-#define IMG_URL "http://ece252-1.uwaterloo.ca:2520/image?img=1"
-#define DUM_URL "https://example.com/"
-#define ECE252_HEADER "X-Ece252-Fragment: "
-#define BUF_SIZE 1048576  /* 1024*1024 = 1M */
-#define BUF_INC  524288   /* 1024*512  = 0.5M */
-
-size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
 size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata);
+size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
 int recv_buf_init(RECV_BUF *ptr, size_t max_size);
 int recv_buf_cleanup(RECV_BUF *ptr);
 int write_file(const char *path, const void *in, size_t len);
--- lab2/src/paster.c
+++ lab2/src/paster.c
@@ -1,5 +1,16 @@
-#include "paster.h"
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h> 
+#include <string.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "png_op.h"
+#include "main_write_header_cb.h"
+
 
 
 #define NUM_STRIPS 50
@@ -15,157 +26,99 @@ int total_download = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 
+/* --------------------------- fetch_worker --------------------------- */
 void *fetch_worker(void *arg) {
-    int img_num = *((int*) arg);
+    int img_num = *((int *)arg);
+    free(arg);       
 
-    CURL *curl_handle = curl_easy_init();
+    unsigned int seed = (unsigned int)(time(NULL) ^ pthread_self());
+
+    CURL *curl = curl_easy_init();
+    if (!curl) pthread_exit(NULL);
+
     RECV_BUF recv_buf;
     recv_buf_init(&recv_buf, BUF_SIZE);
 
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb_curl3);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &recv_buf);
-    curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, header_cb_curl);
-    curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, &recv_buf);
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb_curl3);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &recv_buf);
+    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_cb_curl);
+    curl_easy_setopt(curl, CURLOPT_HEADERDATA, &recv_buf);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 2L); 
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L); 
+
+    int fail_count = 0;
 
     while (1) {
         pthread_mutex_lock(&lock);
-        if(total_download >= 50) {
+        if (total_download >= NUM_STRIPS) {
             pthread_mutex_unlock(&lock);
             break;
         }
         pthread_mutex_unlock(&lock);
 
-        int sidx = rand() % 3;
+        int sidx = rand_r(&seed) % 3;
         char url[256];
-        sprintf(url, "%s/image?img=%d", servers[sidx], img_num);
-        curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+        snprintf(url, sizeof(url), "%s/image?img=%d", servers[sidx], img_num);
+        curl_easy_setopt(curl, CURLOPT_URL, url);
 
         recv_buf.size = 0;
-        recv_buf.seq = -1;
+        recv_buf.seq  = -1;
 
-        CURLcode res = curl_easy_perform(curl_handle);
-        if (res != CURLE_OK || recv_buf.seq < 0 || recv_buf.seq >= 50) {
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK || recv_buf.seq < 0 || recv_buf.seq >= NUM_STRIPS) {
+            if (++fail_count > 1000) break;
             continue;
-        }
+        }  
+
+        int need_write = 0;
+        char fname[64];
+        unsigned char *buf_copy = NULL;
+        size_t size_copy = 0;
 
         pthread_mutex_lock(&lock);
-        if(!downloaded[recv_buf.seq]) {
-            char fname[64];
-            sprintf(fname, "output/output_%d.png", recv_buf.seq);
-            write_file(fname, recv_buf.buf, recv_buf.size);
+
+        if (!downloaded[recv_buf.seq]) {
             downloaded[recv_buf.seq] = 1;
-            total_download ++;
-            //printf("Got strip %d (%d/50)\n", recv_buf.seq, total_download);
+            total_download++;
+            printf("Got strip %d, %d remaining\n",
+             recv_buf.seq, NUM_STRIPS - total_download); 
+            fail_count = 0;
+            size_copy = recv_buf.size;
+            buf_copy = malloc(size_copy);
+
+            if (buf_copy) {
+                memcpy(buf_copy, recv_buf.buf, size_copy);
+                snprintf(fname, sizeof(fname), "output/output_%d.png", recv_buf.seq);
+                need_write = 1;
+            } else {
+                downloaded[recv_buf.seq] = 0;
+                total_download--;
+            }
+
+        } else {
+            if (++fail_count > 1000) {
+                pthread_mutex_unlock(&lock);
+                break;
+            }
         }
         pthread_mutex_unlock(&lock);
-        //usleep(10000);
+
+        if (need_write && buf_copy) {
+            if (write_file(fname, buf_copy, size_copy) != 0)
+                fprintf(stderr, "write_file failed for %s\n", fname);
+            free(buf_copy);
+        }
     }
 
-    curl_easy_cleanup(curl_handle);
+    curl_easy_cleanup(curl);
     recv_buf_cleanup(&recv_buf);
     pthread_exit(NULL);
 }
 
-size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata)
-{
-    size_t realsize = size * nmemb;
-    RECV_BUF *p = (RECV_BUF *)p_userdata;
- 
-    if (p->size + realsize + 1 > p->max_size) {/* hope this rarely happens */ 
-        /* received data is not 0 terminated, add one byte for terminating 0 */
-        size_t new_size = p->max_size + max(BUF_INC, realsize + 1);   
-        char *q = realloc(p->buf, new_size);
-        if (q == NULL) {
-            perror("realloc"); /* out of memory */
-            return -1;
-        }
-        p->buf = q;
-        p->max_size = new_size;
-    }
-
-    memcpy(p->buf + p->size, p_recv, realsize); /*copy data from libcurl*/
-    p->size += realsize;
-    p->buf[p->size] = 0;
-
-    return realsize;
-}
-
-size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata)
-{
-    int realsize = size * nmemb;
-    RECV_BUF *p = userdata;
-    
-    if (realsize > strlen(ECE252_HEADER) &&
-	strncmp(p_recv, ECE252_HEADER, strlen(ECE252_HEADER)) == 0) {
-
-        /* extract img sequence number */
-	p->seq = atoi(p_recv + strlen(ECE252_HEADER));
-
-    }
-    return realsize;
-}
-
-int recv_buf_init(RECV_BUF *ptr, size_t max_size)
-{
-    void *p = NULL;
-    
-    if (ptr == NULL) {
-        return 1;
-    }
-
-    p = malloc(max_size);
-    if (p == NULL) {
-	return 2;
-    }
-    
-    ptr->buf = p;
-    ptr->size = 0;
-    ptr->max_size = max_size;
-    ptr->seq = -1;              /* valid seq should be non-negative */
-    return 0;
-}
-
-int recv_buf_cleanup(RECV_BUF *ptr)
-{
-    if (ptr == NULL) {
-	return 1;
-    }
-    
-    free(ptr->buf);
-    ptr->size = 0;
-    ptr->max_size = 0;
-    return 0;
-}
-
-int write_file(const char *path, const void *in, size_t len) {
-    FILE *fp = NULL;
-
-    if (path == NULL) {
-        fprintf(stderr, "write_file: file name is null!\n");
-        return -1;
-    }
-
-    if (in == NULL) {
-        fprintf(stderr, "write_file: input data is null!\n");
-        return -1;
-    }
-
-    fp = fopen(path, "wb");
-    if (fp == NULL) {
-        perror("fopen");
-        return -2;
-    }
-
-    if (fwrite(in, 1, len, fp) != len) {
-        fprintf(stderr, "write_file: imcomplete write!\n");
-        return -3; 
-    }
-    return fclose(fp);
-}
-
 int main(int argc, char** argv) {
-    mkdir("output", 0777); 
+    if (mkdir("output", 0777) && errno != EEXIST) return 1;
     int t = 1;
     int n = 1;
 
@@ -183,7 +136,13 @@ int main(int argc, char** argv) {
     pthread_t threads[t];
 
     for(int i = 0; i < t; ++i) {
-        int rc = pthread_create(&threads[i], NULL, fetch_worker, &n);
+        int* img_arg = malloc(sizeof(int));
+        if (!img_arg) {
+            perror("malloc");
+            exit(1);
+        }
+        *img_arg = n; 
+        int rc = pthread_create(&threads[i], NULL, fetch_worker, img_arg);
         if (rc) {
         fprintf(stderr, "Failed to create thread. Error code: %d\n", rc);
             return 1;
@@ -205,8 +164,18 @@ int main(int argc, char** argv) {
         sprintf(filenames[i], "output/output_%d.png", i);
     }
 
+    if (total_download != NUM_STRIPS) {
+        fprintf(stderr, "Only got %d/%d strips, aborting cat_png.\n",
+                total_download, NUM_STRIPS);
+        exit(EXIT_FAILURE);
+    }
+
     cat_png((const char **)filenames, 50, "all.png");
     system("rm -rf output");
+
+    for (int i = 0; i < NUM_STRIPS; i++) {
+        free(filenames[i]);
+    }
 
     return 0;
 }
